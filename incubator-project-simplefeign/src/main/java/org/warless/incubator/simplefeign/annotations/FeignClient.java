7d188dd6a9ba768fb60812fc67ddbf075704621a
e <fetaxyu@gmail.com>
2019-08-18 14:17:48 +0800

    add subproject simplefeign

---
+++ incubator-project-simplefeign/src/main/java/org/warless/incubator/simplefeign/annotations/FeignClient.java
@@ -0,0 +1,18 @@
+package org.warless.incubator.simplefeign.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * @author yubb
+ * @date 2019-08-16
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@Documented
+public @interface FeignClient {
+
+    String value() default "http://127.0.0.1";
+
+    Class<? extends Annotation> annotation() default Annotation.class;
+
+}
