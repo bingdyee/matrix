f33fea4bc95733438ffc04f37835826e489ac189
e <fetaxyu@gmail.com>
2019-08-24 10:05:22 +0800

    add TestController

---
+++ incubator-project-simplefeign/src/main/java/org/warless/incubator/simplefeign/controller/TestController.java
@@ -0,0 +1,17 @@
+package org.warless.incubator.simplefeign.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author : fetaxyu
+ * @date : 2019-08-23
+ */
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+
+
+}
