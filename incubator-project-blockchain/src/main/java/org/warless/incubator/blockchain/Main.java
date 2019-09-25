d25194f4e0bac31017bf7edb0c057e13fd34b970
e <fetaxyu@gmail.com>
2019-09-25 12:08:14 +0800

    create block chain project

---
+++ incubator-project-blockchain/src/main/java/org/warless/incubator/blockchain/Main.java
@@ -0,0 +1,13 @@
+package org.warless.incubator.blockchain;
+
+/**
+ * @author : fetaxyu
+ * @date : 2019-09-25
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
