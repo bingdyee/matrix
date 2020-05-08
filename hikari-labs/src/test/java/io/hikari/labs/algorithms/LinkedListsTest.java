b52e164b3cb04ebdc53aac92c9eaf7c5c5eff90f
Noa Swartz <fetaxyu@gmail.com>
2020-05-08 19:11:27 +0800

    using jmh

--- hikari-labs/src/test/java/io/hikari/labs/algorithms/LinkedListsTest.java
+++ hikari-labs/src/test/java/io/hikari/labs/algorithms/LinkedListsTest.java
@@ -69,6 +69,14 @@ public class LinkedListsTest {
         LinkedLists.Node head = createLoopLinkedList();
         System.err.println(LinkedLists.loopLinkedLength(head));
         Assert.assertEquals(LinkedLists.loopLinkedLength(head), 3);
+        /**
+         * 由于扩容是按两倍进行扩，即 N 扩为 N + N，因此就会存在低位部分 0 - (N-1)，以及高位部分 N - (2N-1)， 所以这里分为 loHead (low Head) 和 hiHead (high head)。
+         *
+         * 通过上面的分析，不难发现循环的产生是因为新链表的顺序跟旧的链表是完全相反的，所以只要保证建新链时还是按照原来的顺序的话就不会产生循环。
+         *
+         * JDK8是用 head 和 tail 来保证链表的顺序和之前一样，这样就不会产生循环引用（JDK8以前是颠倒顺序的）。
+         *
+         */
     }
 
 }
