ce296baeb43bd5cbf716811b6f6db60fa10b9280
“vbintx <fetaxyu@gmail.com>
2020-09-26 12:55:05 +0800

    auto get plugin version

--- matrix-gradle-plugin/src/main/java/io/matrix/gradle/dependencymanagement/MatrixPlugin.java
+++ matrix-gradle-plugin/src/main/java/io/matrix/gradle/dependencymanagement/MatrixPlugin.java
@@ -5,6 +5,8 @@ import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.springframework.boot.gradle.plugin.SpringBootPlugin;
+import org.springframework.boot.gradle.util.VersionExtractor;
+
 
 /**
  * @author Noa Swartz
@@ -12,14 +14,15 @@ import org.springframework.boot.gradle.plugin.SpringBootPlugin;
  */
 public class MatrixPlugin implements Plugin<Project> {
 
-    static final String BOM_COORDINATES = "io.matrix:matrix-parent:1.0.0-SNAPSHOT";
+    static final String BOM_COORDINATES = "io.matrix:matrix-parent:";
 
     @Override
     public void apply(final Project project) {
-        new DependencyManagementPlugin().apply(project);
-        new SpringBootPlugin().apply(project);
+        project.getPlugins().apply(DependencyManagementPlugin.class);
+        project.getPlugins().apply(SpringBootPlugin.class);
+        String version = VersionExtractor.forClass(MatrixPlugin.class);
         project.getExtensions().findByType(DependencyManagementExtension.class)
-                .imports((importsHandler) -> importsHandler.mavenBom(MatrixPlugin.BOM_COORDINATES));
+                .imports((importsHandler) -> importsHandler.mavenBom(MatrixPlugin.BOM_COORDINATES + version));
     }
 
 }
