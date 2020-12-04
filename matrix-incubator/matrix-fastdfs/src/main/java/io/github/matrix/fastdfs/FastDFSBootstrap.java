c40eaac0b6ff6ff3672da10e3a3cc6347b0448d4
“vbintx <fetaxyu@gmail.com>
2020-12-04 13:55:52 +0800

    FastDFS upload test

---
+++ matrix-incubator/matrix-fastdfs/src/main/java/io/github/matrix/fastdfs/FastDFSBootstrap.java
@@ -0,0 +1,29 @@
+package io.github.matrix.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * @author Noa Swartz
+ * @date 2020/12/04
+ */
+@SpringBootApplication
+public class FastDFSBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastDFSBootstrap.class, args);
+    }
+
+}
