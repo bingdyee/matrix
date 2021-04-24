4954d8abf15926b946f0eddf8d073025654ca756
Bing D. Yee <bingdyee@gmail.com>
2021-04-24 18:05:03 +0800

    spring boot starter

---
+++ matrix-tools/matrix-spring-boot-starter/src/main/java/io/github/matrix/boot/MatrixAutoConfiguration.java
@@ -0,0 +1,14 @@
+package io.github.matrix.boot;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Bing D. Yee
+ * @since 2021/04/24
+ */
+@Configuration
+public class MatrixAutoConfiguration {
+
+
+
+}
